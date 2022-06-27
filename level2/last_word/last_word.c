b717d39605d92a730226bb3ffe213421e0e40b4d
galdor30 <dsjaramillo@hotmail.com>
2022-06-27 16:08:36 +0200

    level 1 and 2 finished

--- level2/last_word/last_word.c
+++ level2/last_word/last_word.c
@@ -1,24 +1,23 @@
 #include <unistd.h>
 
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-    int     i;
+    int i = 0;
 
-    i = 0;
-    if (argc == 2)
+    if (ac == 2)
     {
-        while (argv[1][i] != '\0')
+        while (av[1][i] != '\0')
             i++;
         i--;
-        while ((argv[1][i] != ' ' && argv[1][i] != '\t') && i >= 0)
+        while (av[1][i] != ' ' && av[1][i] != '\t')
             i--;
-        i++;
-        while (argv[1][i] != '\0')
-        {
-            write(1, &argv[1][i], 1);
-            i++;
-        }
+		i++;
+        while (av[1][i])
+		{
+			write(1,&av[1][i], 1);
+			i++;
+		}
     }
-    write(1, "\n", 1);
+    write (1, "\n", 1);
     return (0);
 }
